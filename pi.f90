aa2e1dbe6afded23ecd4e70c6b4d40ff5eb3529c
Guruprasad S <61557979+Guru112358@users.noreply.github.com>
2020-03-26 12:37:17 +0530

    Add files via upload

---
+++ pi.f90
@@ -0,0 +1,25 @@
+program monte
+!this program computes the value of Pi approximately by employing a Monte carlo method.
+!the random__number module is called to produce random points between 0 and 1 in x and y coordinates of a square region of space 1 X 1.
+!if they fall within a unit circle,the point is counted.
+!increasing number of random points increases accuracy.
+!this method is limted by the quality of the pseudorandom number generator and number of points the memory can handle.
+implicit none
+real::x,y,r,pi
+integer::n,i,c
+c=0 !counter
+n=10000!number of random points
+open(1,file="pi12.dat",status="old")
+do i=1,n
+call random_number(x)
+call random_number(y)
+r=(x**2+y**2)**0.5
+if(r<=1)then
+c=c+1 !incrementing each time the random point falls within the circle
+pi=4.0*real(c)/(i)
+print *,i,pi
+write(1,*)i,x,y,r,pi
+end if
+end do
+close(1)
+end program
