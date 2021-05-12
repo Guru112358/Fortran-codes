6e2c73cdda7145910fea7b5592c42302e7d52e85
Guruprasad S <61557979+Guru112358@users.noreply.github.com>
2021-05-12 18:49:07 +0530

    Update projectile.f90

--- projectile.f90
+++ projectile.f90
@@ -1,9 +1,8 @@
-
 program Projectile
-    !this program solves for the trajecttory of a projectile with quadratic air resistance using a second order modified Euler Scheme.
+    !this program solves for the trajecttory of a projectile with quadratic air resistance  Euler Scheme.
     implicit none
     double precision,parameter::pi=4*ATAN(1.0)
-    double precision::v0,vx0,vy0,vx,vy,theta,x,y,x0,y0,dt,vxhalf,vyhalf
+    double precision::v0,vx0,vy0,vx,vy,theta,x,y,x0,y0,dt,vxbar,vybar,vxnp1,vynp1,xnp1,ynp1,xbar,ybar
     double precision::m  !mass of the projectile
     double precision::c   !drag coefficient
     double precision,parameter::g=9.81 !acceleration due to gravity
@@ -31,7 +30,7 @@ program Projectile
     vx0=v0*COS(theta*(pi/180))  !x velocity
     vy0=v0*SIN(theta*(pi/180))  ! y velocity
     t=0
-    switch1=1
+    switch1=0
 
     open(1,file='xyproj.dat',status='replace')
     open(2,file='xyproj.plt',status='replace')
@@ -39,25 +38,24 @@ program Projectile
 write(*,*)"please wait,computing trajectory......"
 write(*,*)
 
-do while(switch1.GT.0)
+do while(switch1.EQ.0)
         t=t+1  
-        vxhalf=vx0+(dvx(vx0,vy0,m,c,g))*(dt/2)
-        vyhalf=vy0+(dvy(vx0,vy0,m,c,g))*(dt/2)
-        vx=vx0+(dvx(vxhalf,vyhalf,m,c,g))*(dt)
-        vy=vy0+(dvy(vxhalf,vyhalf,m,c,g))*(dt)
-        x=x0+(vx*dt/2)
-        y=y0+(vy*dt/2)
-        vx0=vxhalf
-        vy0=vyhalf
-        x0=x
-        y0=y
-        if(y<=0)then  !condition to halt the program after the projectile touches the ground(y=0)
-        switch1=-1
+        vxnp1=vx0+dvx(vx0,vy0,m,c,g)*(dt)
+        vynp1=vy0+dvy(vx0,vy0,m,c,g)*(dt)
+        xnp1=x0+(vx0*dt)+(0.5*dt**2)*(dvx(vx0,vy0,m,c,g))
+        ynp1=y0+(vy0*dt)+(0.5*dt**2)*(dvy(vx0,vy0,m,c,g))
+        x0=xnp1
+        y0=ynp1
+        vx0=vxnp1
+        vy0=vynp1
+
+        if(ynp1<=0)then  !condition to halt the program after the projectile touches the ground(y=0)
+        switch1=1
          write(*,*)"Integration complete!,plotting results"
          write(*,*)"Time of flight:",t*dt,"seconds"
         else
-        !write(*,*)x,y
-        write(1,*)x,y
+        write(*,*)xnp1,ynp1
+        write(1,*)xnp1,ynp1
       
        end if   
  end do
