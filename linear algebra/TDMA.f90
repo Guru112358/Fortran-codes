de8f4192abf5ef6c9733644e86db6d14e6be9e49
Guruprasad S <61557979+Guru112358@users.noreply.github.com>
2021-07-29 20:44:43 +0530

    Update TDMA.f90

--- linear algebra/TDMA.f90
+++ linear algebra/TDMA.f90
@@ -4,8 +4,8 @@ program Thomas_algorithm
     implicit none
     integer,parameter::n=5
     integer::i
-    real,dimension(n)::a,b,c,d,x
-    real,dimension(n)::gammar,beta
+    real,dimension(0:n)::a,b,c,d,x
+    real,dimension(0:n)::p,q
 
 !!!!!!!!!taking diagonals as input!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -15,39 +15,42 @@ write(*,*)'b= '
 write(*,*)(b(i),i=1,n)
 
 write(*,*) "enter the lower diagonal elements(a)"
-read *,(a(i),i=1,n)
+read *,(a(i),i=2,n)
 write(*,*)'a= '
-write(*,*)(a(i),i=1,n)
+write(*,*)(a(i),i=2,n)
 
 write(*,*) "enter the upper diagonal elements(c)"
-read *,(c(i),i=1,n)
+read *,(c(i),i=1,n-1)
 write(*,*)'c= '
-write(*,*)(c(i),i=1,n)
+write(*,*)(c(i),i=1,n-1)
 
 write(*,*)"enter the RHS matrix(d)"
 read *,(d(i),i=1,n)
 write(*,*)'d= '
 write(*,*)(d(i),i=1,n)
+a(1)=0
+ c(n)=0
+
+
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Algorithm !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !forward sweep
- gammar(1) =c(1)/b(1)
- beta(1)= d(1)/b(1)
+ p(1) =-c(1)/b(1)
+ q(1)= d(1)/b(1)
 
 do i=2,n
 
-    gammar(i)=c(i)/(b(i)-(a(i)*gammar(i-1)))
-    beta(i)=(d(i)-a(i)*beta(i-1))/(b(i)-a(i)*gammar(i-1))
+    p(i)=-c(i)/(b(i)+(a(i)*p(i-1)))
+    q(i)=(d(i)-a(i)*q(i-1))/(b(i)+a(i)*p(i-1))
 
 end do
 !backward sweep
-x(n)=beta(n)
+x(n)=q(n)
 do i=(n-1),1,-1
-    x(i)=beta(i)-gammar(i)*x(i+1)
+    x(i)=q(i)+p(i)*x(i+1)
 end do
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-write(*,*),'the solution matrix x is: '
-write(*,*),(x(i),i=1,n)
-
+write(*,*)'the solution matrix x is: '
+write(*,*)(x(i),i=1,n)
 
 end program Thomas_algorithm
